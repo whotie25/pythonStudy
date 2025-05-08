f4dc892b65ec1688ffadccde980dee7b351f2b76
DESKTOP-5MNKAEQ\nam <whotie506@gmail.com>
2025-05-08 22:11:49 +0900

    mergeSort Clear

--- mergeSort.py
+++ mergeSort.py
@@ -1,1 +1,27 @@
-seq = []
+def mergeSort(_seq):
+    if (len(_seq) <= 1):
+        return _seq
+    
+    mid = len(_seq)//2 #division
+    l = _seq[:mid]
+    r = _seq[mid:]
+
+    _l = mergeSort(l)
+    _r = mergeSort(r)
+    return conquer(_l, _r)
+
+def conquer(_l, _r):
+    l = r = 0
+    res = []
+
+    while (l < len(_l) and r < len(_r)):
+        if(_l[l] < _r[r]): res.append(_l[l]); l += 1
+        else: res.append(_r[r]); r += 1
+    while l < len(_l): res.append(_l[l]); l += 1
+    while r < len(_r): res.append(_r[r]); r += 1
+
+    return res
+
+seq = [4, 5, 2, 7, 9]
+
+print(mergeSort(seq))
