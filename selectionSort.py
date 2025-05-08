35d55a2ee12e9dae7dc198b608c9f3e0cbb7b814
DESKTOP-5MNKAEQ\nam <whotie506@gmail.com>
2025-05-08 23:12:15 +0900

    selection sort clear

---
+++ selectionSort.py
@@ -0,0 +1,7 @@
+def selectionSort(_seq):
+    for i in range(len(_seq) - 1):
+        min_idx = i
+        for j in range(i + 1, len(_seq)):
+            if(_seq[j] < _seq[min_idx]): min_idx = j
+        _seq[i], _seq[min_idx] = _seq[min_idx], _seq[i]
+    return _seq
